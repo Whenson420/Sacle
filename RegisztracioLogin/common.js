e74120d8decfc4d5c821947089f3331481010c10
Whenson420 <73558023+Whenson420@users.noreply.github.com>
2023-11-15 18:17:37 +0100

    3.

--- RegisztracioLogin/common.js
+++ RegisztracioLogin/common.js
@@ -1,6 +1,7 @@
 const signUpButton = document.getElementById('signUp');
 const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
+pwShowHide = document.querySelectorAll(".pw_hide");
 
 signUpButton.addEventListener('click', () => {
     container.classList.add("right-panel-active");
@@ -8,4 +9,22 @@ signUpButton.addEventListener('click', () => {
 
 signInButton.addEventListener('click', () => {
     container.classList.remove("right-panel-active");
+});
+function changeTitleReg() {
+    document.title = "Sacle - Regisztráció";
+}
+function changeTitleLog() {
+    document.title = "Sacle - Bejelentkezés";
+}
+pwShowHide.forEach((icon) => {
+    icon.addEventListener("click", () => {
+        let getPwInput = icon.parentElement.querySelector("input");
+        if (getPwInput.type === "password") {
+            getPwInput.type = "text";
+            icon.classList.replace("uil-eye-slash", "uil-eye");
+        } else {
+            getPwInput.type = "password";
+            icon.classList.replace("uil-eye", "uil-eye-slash");
+        }
+    });
 });
