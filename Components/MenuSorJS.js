f5f81bc03928f8ce81f9771625f6a09de34a1d30
Whenson420 <73558023+Whenson420@users.noreply.github.com>
2024-02-07 13:16:09 +0100

    worst

--- Components/MenuSorJS.js
+++ Components/MenuSorJS.js
@@ -1,4 +1,16 @@
 fetch('/Components/MenuSor.html')
+    .then(function(response) {
+        // Set the Content-Type header to text/javascript
+        response.headers.set('Content-Type', 'text/javascript');
+
+        // Convert the response to text
+        return response.text();
+    })
+    .then(function(menu) {
+        console.log(menu);
+        document.getElementById('Navigation').innerHTML = menu;
+    });
+fetch('/Components/MenuSor.html')
     .then(function(res){return res.text();})
     .then((menu)=>{
         console.log(menu);
